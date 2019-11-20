2e42953eb0f0c276a0dc5fa1771d2661acdccbfc
Lorenzo Pinto <lorenzo-pinto@hotmail.it>
2019-11-20 14:37:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,62 +48,67 @@ def predict():
     # route http posts to this method
 @app.route('/api/test', methods=['POST', 'GET'])
 def test():
-    if request.method == 'POST':
-    
-        r = request
-        #return str(r.files['file_field'])
-        #return(str(r.files))
-        #nparr = np.asarray(Image.open(r.files['file_field']))
-        img = Image.open(r.files['file_field'])
-        #nparr = np.frombuffer(r.files['file_field'], np.uint8)
-        
-        image = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-        
-        #return(str(img))
-        
-        # decode image
-        #image = cv2.imdecode(img, cv2.IMREAD_COLOR)
-        cv2.imwrite('image.jpg', image)
-        #cv2.imwrite('file.jpg', image)
-        # our CNN requires fixed spatial dimensions for our input image(s)
-        # so we need to ensure it is resized to 224x224 pixels while
-        # performing mean subtraction (104, 117, 123) to normalize the input;
-        # after executing this command our "blob" now has the shape:
-        # (1, 3, 224, 224)
-        blob = cv2.dnn.blobFromImage(image, 1, (224, 224), (104, 117, 123))
-        # set the blob as input to the network and perform a forward-pass to
-        # obtain our output classification
-        net.setInput(blob)
-        preds = net.forward()
+    try:
+        if request.method == 'POST':
 
-        # sort the indexes of the probabilities in descending order (higher
-        # probabilitiy first) and grab the top-5 predictions
-        idxs = np.argsort(preds[0])[::-1][:5]
-        listResults = []
-        # loop over the top-5 predictions and display them
-        for (i, idx) in enumerate(idxs):
-            # draw the top prediction on the input image
-            if i == 0:
-                text = "Label: {}, {:.2f}%".format(classes[idx],
-                    preds[0][idx] * 100)
-                cv2.putText(image, text, (5, 25),  cv2.FONT_HERSHEY_SIMPLEX,
-                    0.7, (0, 0, 255), 2)
+            r = request
+            #return str(r.files['file_field'])
+            #return(str(r.files))
+            #nparr = np.asarray(Image.open(r.files['file_field']))
+            img = Image.open(r.files['file_field'])
+            #nparr = np.frombuffer(r.files['file_field'], np.uint8)
 
-            # display the predicted label + associated probability to the
-            # console
-            output = ("{}. label: {}, probability: {:.5}".format(i + 1,
-                classes[idx], preds[0][idx]))
-            listResults.append(output)
-            
+            image = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
-        response = {'results' : listResults}
-        #response = {'message': 'image received'}
-        # encode response using jsonpickle
-        response_pickled = jsonpickle.encode(response)
-        #return (response)
-        return Response(response=response_pickled, status=200, mimetype="application/json")
-    else:
-        return ('richiesta non in post')
+            #return(str(img))
+
+            # decode image
+            #image = cv2.imdecode(img, cv2.IMREAD_COLOR)
+            cv2.imwrite('image.jpg', image)
+            #cv2.imwrite('file.jpg', image)
+            # our CNN requires fixed spatial dimensions for our input image(s)
+            # so we need to ensure it is resized to 224x224 pixels while
+            # performing mean subtraction (104, 117, 123) to normalize the input;
+            # after executing this command our "blob" now has the shape:
+            # (1, 3, 224, 224)
+            blob = cv2.dnn.blobFromImage(image, 1, (224, 224), (104, 117, 123))
+            # set the blob as input to the network and perform a forward-pass to
+            # obtain our output classification
+            net.setInput(blob)
+            preds = net.forward()
+
+            # sort the indexes of the probabilities in descending order (higher
+            # probabilitiy first) and grab the top-5 predictions
+            idxs = np.argsort(preds[0])[::-1][:5]
+            listResults = []
+            # loop over the top-5 predictions and display them
+            for (i, idx) in enumerate(idxs):
+                # draw the top prediction on the input image
+                if i == 0:
+                    text = "Label: {}, {:.2f}%".format(classes[idx],
+                        preds[0][idx] * 100)
+                    cv2.putText(image, text, (5, 25),  cv2.FONT_HERSHEY_SIMPLEX,
+                        0.7, (0, 0, 255), 2)
+
+                # display the predicted label + associated probability to the
+                # console
+                output = ("{}. label: {}, probability: {:.5}".format(i + 1,
+                    classes[idx], preds[0][idx]))
+                listResults.append(output)
+
+
+            response = {'results' : listResults}
+            #response = {'message': 'image received'}
+            # encode response using jsonpickle
+            response_pickled = jsonpickle.encode(response)
+            #return (response)
+            return Response(response=response_pickled, status=200, mimetype="application/json")
+        else:
+            return ('richiesta non in post')
+    except Exception as e:
+        response = {'results' : str(e)}
+        
+        
 
 
 
